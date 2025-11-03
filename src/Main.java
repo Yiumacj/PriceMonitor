819bc0369466056e4e8f4641358680ffa83500e2
Yiumacj <trackop.com@gmail.com>
2025-11-03 13:06:05 +0500

    + Hibernate ORM

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ import view.ConsoleView;
 void main() {
     /*Todo
     + Add logger
+    + Add Test
+    + Add interface between api, db
+    + Add 
      */
     testCore.run(testVariations.ALL);
     ConsoleView bot = new ConsoleView();
