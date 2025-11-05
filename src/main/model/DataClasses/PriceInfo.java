2741803b3b3188a3603fad59131391b134bd7945
Yiumacj <trackop.com@gmail.com>
2025-11-05 16:20:49 +0500

    Adapted dataclasses to new ORM

--- src/main/model/DataClasses/PriceInfo.java
+++ src/main/model/DataClasses/PriceInfo.java
@@ -1,15 +1,20 @@
-package model.DataClasses;
+package main.model.DataClasses;
 
-import interfaces.model.IDataBaseItem;
+import main.interfaces.model.IDataBaseObject;
+import jakarta.persistence.*;
 
-public class priceInfo implements IDataBaseItem {
-    private int id = 0;
+@Entity
+@Table(name = "priceinfo")
+public class PriceInfo implements IDataBaseObject {
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id = 0;
     private double finalPrice = 0;
     private double initialPrice = 0;
     private int discountPercent = 0;
     private String currency = "";
 
-    public priceInfo(int id, double finalPrice, double initialPrice, int discountPercent, String currency) {
+    public PriceInfo(int id, double finalPrice, double initialPrice, int discountPercent, String currency) {
         this.id = id;
         this.finalPrice = finalPrice;
         this.initialPrice = initialPrice;
@@ -17,10 +22,9 @@ public class priceInfo implements IDataBaseItem {
         this.currency = currency;
     }
 
-    public priceInfo() {}
+    public PriceInfo() {}
 
     // Геттеры
-    @Override
     public int getId() { return id; }
     public double getFinalPrice() { return finalPrice; }
     public double getInitialPrice() { return initialPrice; }
