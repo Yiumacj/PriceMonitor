7679bdaea9249ede98ebf025ff36a5f1b1845688
Yiumacj <trackop.com@gmail.com>
2025-11-07 10:01:00 +0500

    Merge fix

--- src/main/model/DataClasses/AddQueryStatus.java
+++ src/main/model/DataClasses/AddQueryStatus.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/main/model/dataClasses/AddQueryStatus.java
-package main.model.dataClasses;
-========
 package main.model.DataClasses;
->>>>>>>> origin/Update-0.2:src/main/model/DataClasses/AddQueryStatus.java
 
 public enum AddQueryStatus
 {
---
+++ src/main/model/DataClasses/PriceInfo.java
@@ -0,0 +1,33 @@
+package main.model.DataClasses;
+
+import main.interfaces.model.IDataBaseObject;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "priceinfo")
+public class PriceInfo implements IDataBaseObject {
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id = 0;
+    private double finalPrice = 0;
+    private double initialPrice = 0;
+    private int discountPercent = 0;
+    private String currency = "";
+
+    public PriceInfo(int id, double finalPrice, double initialPrice, int discountPercent, String currency) {
+        this.id = id;
+        this.finalPrice = finalPrice;
+        this.initialPrice = initialPrice;
+        this.discountPercent = discountPercent;
+        this.currency = currency;
+    }
+
+    public PriceInfo() {}
+
+    // Геттеры
+    public int getId() { return id; }
+    public double getFinalPrice() { return finalPrice; }
+    public double getInitialPrice() { return initialPrice; }
+    public int getDiscountPercent() { return discountPercent; }
+    public String getCurrency() { return currency; }
+}
